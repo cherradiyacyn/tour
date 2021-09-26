d4aa406498f41efc12fb5f691ba91748317b7833
Yacyn CHERRADI <cherradiyacyn@gmail.com>
2021-09-26 19:53:54 +0100

    first commit

---
+++ 1_basics/2_flow_control/defer-multi.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Deferred function calls are pushed onto a stack. When a function returns, its deferred calls are executed in last-in-first-out order.
+	fmt.Println("counting")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
+}
