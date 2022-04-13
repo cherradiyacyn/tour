e6b08d8cfff0eeef4b278f9404d36a03fb085c01
Yacyn CHERRADI <cherradiyacyn@gmail.com>
2022-04-13 00:09:53 +0000

    first commit

--- 2_methods_and_interfaces/type-switches.go
+++ 2_methods_and_interfaces/type-switches.go
@@ -5,9 +5,9 @@ import "fmt"
 func do(i interface{}) {
 	switch v := i.(type) {
 	case int:
-		fmt.Printf("Twice %v is %v!\n", v, v*2)
+		fmt.Printf("Twice %v is %v\n", v, v*v)
 	case string:
-		fmt.Printf("%q is %v bytes long!\n", v, len(v))
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
 	default:
 		fmt.Printf("I don't know about type %T!\n", v)
 	}
